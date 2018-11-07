893dc3bbfbd431b1410b4efd7d0a8a2807bc39b3
abc234466 <abc2344666@gmail.com>
2018-11-07 23:52:11 +0800

    Add Linked List Node

---
+++ stack+LinkedList.py
@@ -0,0 +1,47 @@
+class LinkedNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.base = None
+    def isEmpty(self):
+        return True if self.base == None else False
+    def push(self, data):
+        newNode = LinkedNode(data)
+        newNode.next = self.base
+        self.base = newNode
+        print(f"{data} pushed into stack\n")
+    def pop(self):
+        if self.isEmpty():
+            return float("-inf")
+        popdata = self.base.data
+        self.base = self.base.next
+        print(f"{popdata} popped from stack\n")
+        return popdata
+    def peek(self):
+        if self.base != None:
+            return self.base.data
+        else:
+            print("Nothing in the stack\n")
+            return float("-inf")
+    def printStack(self):
+        print("==Total itmes in the Stack==")
+        temp = self.base
+        while temp != None:
+            print(temp.data)
+            temp = temp.next
+        print("== END ==")
+# TEST
+stack = Stack()
+stack.peek()
+stack.push(5)
+stack.push(1)
+stack.push(3)
+stack.printStack()
+stack.pop()
+
+
+            
+    
