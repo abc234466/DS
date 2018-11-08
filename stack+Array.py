be9b86f549e7cf328ad8e9cb41c874495844029e
abc234466 <abc2344666@gmail.com>
2018-11-09 00:49:51 +0800

    Update stack+array

--- stack+Array.py
+++ stack+Array.py
@@ -3,18 +3,20 @@
 class Stack:
     def __init__(self):
         self.stack = []
+        self.minstack = []
         self.min = float('inf')
 
     def isEmpty(self):
         return len(self.stack) == 0
 
-    def getmin(self):
-        print("Min Value in stack : ", self.min, "\n")
+    def getMin(self):
+        print("Min Value in stack : ", self.minstack[-1], "\n")
         return self.min
     def push(self, data):
         if data < self.min :
             self.min = data
         self.stack.append(data)
+        self.minstack.append(self.min)
         print(f"{data} pushed into stack")
         print(f"Stack = {self.stack}\n")
 
@@ -23,7 +25,8 @@ class Stack:
             print(f"Nothing in the stack\n")
             return
         popdata = self.stack[-1]
-        self.stack = self.stack[:len(self.stack)-1]
+        self.stack = self.stack[:-1]
+        self.minstack = self.minstack[:-1]
         print(f"{popdata} popped from stack")
         print(f"Stack = {self.stack}\n")
 
@@ -31,9 +34,9 @@ class Stack:
 stack = Stack()
 stack.pop()
 stack.push(5)
-stack.push(1)
+stack.push(4)
 stack.push(3)
 stack.push(2)
-stack.getmin()
+stack.getMin()
 stack.pop()
-           
+stack.getMin()
